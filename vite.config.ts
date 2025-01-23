da058f4ec73e9329a79892107bf19ad09936a951
yangheng <492238647@qq.com>
2025-01-23 16:08:59 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/leafer-greedy-snake/',
+  // 打包文件夹
+  build: {
+    outDir: 'docs'
+  },
   plugins: [vue()],
 })
